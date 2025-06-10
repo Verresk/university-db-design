f78c4ea21a69920fa54f0e7be1cc624c89586c19
Alpinist282 <nurpiisov.nikita282@gmail.com>
2025-06-10 23:31:56 +0300

    added data check file

---
+++ SQL-scripts/data_check.sql
@@ -0,0 +1,16 @@
+--Скрипт для простых запросов, предназначенных для просмотров всех записей в таблицах
+
+SELECT * FROM Gym;
+SELECT * FROM Payment_method;
+SELECT * FROM Client;
+
+SELECT * FROM Subscriptions;
+SELECT * FROM Employee;
+SELECT * FROM Equipment;
+
+SELECT * FROM Activities;
+
+SELECT * FROM Payment;
+SELECT * FROM Cheque;
+SELECT * FROM Club_card;
+SELECT * FROM Activities_attending;
